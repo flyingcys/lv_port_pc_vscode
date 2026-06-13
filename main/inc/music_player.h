e50660c3734bc0fdd022390348ce77041d8c8bad
chenyisong <chenyisong@tuya.com>
2026-06-13 17:41:13 +0800

    feat: add music_player stub interface

---
+++ main/inc/music_player.h
@@ -0,0 +1,22 @@
+/* main/inc/music_player.h */
+#ifndef MUSIC_PLAYER_H
+#define MUSIC_PLAYER_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+void   music_player_init(const char **urls, size_t count);
+void   music_player_deinit(void);
+
+void   music_player_play(void);
+void   music_player_pause(void);
+void   music_player_resume(void);
+void   music_player_next(void);
+void   music_player_prev(void);
+void   music_player_select(size_t index);
+
+size_t music_player_get_count(void);
+size_t music_player_get_current_index(void);
+bool   music_player_is_playing(void);
+
+#endif /* MUSIC_PLAYER_H */
--- main/src/music_player.c
+++ main/src/music_player.c
@@ -1,1 +1,14 @@
-/* music_player placeholder */
+/* main/src/music_player.c */
+#include "music_player.h"
+
+void   music_player_init(const char **urls, size_t count) { (void)urls; (void)count; }
+void   music_player_deinit(void) {}
+void   music_player_play(void) {}
+void   music_player_pause(void) {}
+void   music_player_resume(void) {}
+void   music_player_next(void) {}
+void   music_player_prev(void) {}
+void   music_player_select(size_t index) { (void)index; }
+size_t music_player_get_count(void) { return 0; }
+size_t music_player_get_current_index(void) { return 0; }
+bool   music_player_is_playing(void) { return false; }
