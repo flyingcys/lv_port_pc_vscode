37cfb31a8e2396aa6632ccc32e76aade19da94a9
flyingcys <flyingcys@163.com>
2026-09-04 22:07:14 +0800

    update web

--- main/src/main.c
+++ main/src/main.c
@@ -15,14 +15,25 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
-#include "lvgl/lvgl.h"
+#include "lvgl.h"
+#if defined(VENUS_CODEGEN_DEBUG)
+#include "ui.h"
+#else
 #include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
+#endif
 #include "glob.h"
 
 /*********************
  *      DEFINES
  *********************/
+/* 分辨率：优先用 codegen 写入 lv_conf.h 的 LV_DISP_DEF_*；未定义时默认 320×480。 */
+#ifndef LV_DISP_DEF_WIDTH
+#define LV_DISP_DEF_WIDTH 320
+#endif
+#ifndef LV_DISP_DEF_HEIGHT
+#define LV_DISP_DEF_HEIGHT 480
+#endif
 
 /**********************
  *      TYPEDEFS
@@ -35,12 +46,15 @@ static lv_display_t * hal_init(int32_t w, int32_t h);
 #ifdef __EMSCRIPTEN__
 static void emscripten_loop(void * arg);
 #endif
+#if defined(VENUS_CODEGEN_DEBUG)
+static void venus_dump_geom_after_layout(void);
+#endif
 
 /**********************
  *  STATIC VARIABLES
  **********************/
 
-/********************** 
+/**********************
  *      MACROS
  **********************/
 
@@ -79,12 +93,21 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  hal_init(320, 480);
+  hal_init(LV_DISP_DEF_WIDTH, LV_DISP_DEF_HEIGHT);
 
-  #if LV_USE_OS == LV_OS_NONE
- 
+  #if defined(VENUS_CODEGEN_DEBUG)
+  ui_create();
+  #elif LV_USE_OS == LV_OS_NONE
   lv_demo_widgets();
 
+  #elif LV_USE_OS == LV_OS_FREERTOS
+
+  /* Run FreeRTOS and create lvgl task */
+  freertos_main();
+
+  #endif
+
+  #if LV_USE_OS == LV_OS_NONE
   #ifdef __EMSCRIPTEN__
   emscripten_set_main_loop_arg(emscripten_loop, NULL, 0, true);
   #else
@@ -92,14 +115,12 @@ int main(int argc, char **argv)
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
     lv_timer_handler();
+    #if defined(VENUS_CODEGEN_DEBUG)
+    venus_dump_geom_after_layout();
+    #endif
     usleep(5 * 1000);
   }
   #endif
-
-  #elif LV_USE_OS == LV_OS_FREERTOS
-
-  /* Run FreeRTOS and create lvgl task */
-  freertos_main();  
 
   #endif
 
@@ -126,11 +147,13 @@ static lv_display_t * hal_init(int32_t w, int32_t h)
   lv_indev_set_display(mouse, disp);
   lv_display_set_default(disp);
 
+  #if !defined(VENUS_CODEGEN_DEBUG)
   LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
   lv_obj_t * cursor_obj;
   cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
   lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
   lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
+  #endif
 
   lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
   lv_indev_set_display(mousewheel, disp);
@@ -148,5 +171,18 @@ static void emscripten_loop(void * arg)
 {
   (void)arg;
   lv_timer_handler();
+  #if defined(VENUS_CODEGEN_DEBUG)
+  venus_dump_geom_after_layout();
+  #endif
+}
+#endif
+
+#if defined(VENUS_CODEGEN_DEBUG)
+static void venus_dump_geom_after_layout(void)
+{
+  static int dumped;
+  if(dumped) return;
+  dumped = 1;
+  venus_ui_dump_geom();
 }
 #endif
